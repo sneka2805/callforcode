0dafbdd426edc4d814cd46078d755d419a9631a0
sneka2805 <162814125+sneka2805@users.noreply.github.com>
2024-10-16 20:00:40 +0530

    jav.js

---
+++ jav.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.loading-btn').addEventListener('click', function() {
+    alert('Loading Complete!');
+Document.querySelector('.btn').addEventListener('click', function() {
+    alert('Button clicked!');
+});
+
+
